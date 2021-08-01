f89506a2100f90ff5613ee1770a7ee292f7b934f
hai <hai@LAPTOP-4DV9N6FJ>
2021-08-01 19:39:29 +0530

    Tenth commit
    Upload all files

---
+++ ARRAY_NARROW/src/s/TEST9.java
@@ -0,0 +1,16 @@
+package s;
+
+public class TEST9 {
+
+	public static void main(String[] args) {
+		int a=1234;
+		byte b;
+		b=(byte)a;
+		System.out.println("a  is " +a);
+		System.out.println("b is " +b);
+		
+		
+
+	}
+
+}
